42d9649b971cee674ebd60b6203c1941dbbae594
Bramsouare <souarei404@gmail.com>
2023-11-30 13:32:59 +0100

    RESSOURCE

---
+++ site.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lien ressourse</title>
+</head>
+<body>
+
+    https://www.site24x7.com/fr/tools/selecteur-de-code-couleur.html
+    https://www.w3schools.com/
+    https://stackoverflow.com/
+    https://www.developpez.com/
+    https://www.codecademy.com/
+    https://www.php.net/manual/fr/intro-whatis.php
+    https://www.learn-php.org/
+    https://dev.mysql.com/doc/
+    https://aymeric-auberton.fr/academie/mysql/
+    https://www.univ-orleans.fr/iut-orleans/informatique/intra/tuto/php/bd-php-pdo.html
+    https://symfony.com/doc/current/index.html
+    https://openclassrooms.com/fr/
+    https://www.geeksforgeeks.org/
+    
+    
+</body>
+</html>
